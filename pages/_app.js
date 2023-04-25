62deab8cac15a66277cc0e219e58afffa82464f6
Darius19920110 <meszidani22@gmail.com>
2023-04-25 10:23:32 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 import HeadMeta from "../src/HeadMeta";
 import State from "../src/context/state";
 import PreLoader from "../src/layout/PreLoader";
@@ -5,14 +9,46 @@ import "../styles/globals.css";
 import { SSRProvider } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <State>
-      <HeadMeta title="Daniel - CV / Resume / Portfolio" />
-      <PreLoader />
-      <SSRProvider>
-        <Component {...pageProps} />
-      </SSRProvider>
-    </State>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-CBTMJ4L7L7"
+      ></Script>
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CBTMJ4L7L7', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <State>
+        <HeadMeta title="Daniel - CV / Resume / Portfolio" />
+        <PreLoader />
+        <SSRProvider>
+          <Component {...pageProps} />
+        </SSRProvider>
+      </State>
+    </>
   );
 }
 
